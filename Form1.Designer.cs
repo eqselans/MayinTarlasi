1cc74abb94d6226e2bbae3241b1d5f078535100f
Emirhan AKSU <112195334+eqselans@users.noreply.github.com>
2023-03-30 17:53:33 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace AynıZamanlıUretim
+{
+	partial class Form1
+	{
+		/// <summary>
+		///Gerekli tasarımcı değişkeni.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///Kullanılan tüm kaynakları temizleyin.
+		/// </summary>
+		///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer üretilen kod
+
+		/// <summary>
+		/// Tasarımcı desteği için gerekli metot - bu metodun 
+		///içeriğini kod düzenleyici ile değiştirmeyin.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.btnUret = new System.Windows.Forms.Button();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblSkor = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.lblMayın = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnUret
+			// 
+			resources.ApplyResources(this.btnUret, "btnUret");
+			this.btnUret.Name = "btnUret";
+			this.btnUret.UseVisualStyleBackColor = true;
+			this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+			// 
+			// flowLayoutPanel1
+			// 
+			resources.ApplyResources(this.flowLayoutPanel1, "flowLayoutPanel1");
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			// 
+			// label1
+			// 
+			resources.ApplyResources(this.label1, "label1");
+			this.label1.Name = "label1";
+			// 
+			// lblSkor
+			// 
+			resources.ApplyResources(this.lblSkor, "lblSkor");
+			this.lblSkor.Name = "lblSkor";
+			// 
+			// label2
+			// 
+			resources.ApplyResources(this.label2, "label2");
+			this.label2.Name = "label2";
+			// 
+			// lblMayın
+			// 
+			resources.ApplyResources(this.lblMayın, "lblMayın");
+			this.lblMayın.Name = "lblMayın";
+			// 
+			// Form1
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.lblMayın);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.lblSkor);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.flowLayoutPanel1);
+			this.Controls.Add(this.btnUret);
+			this.Name = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnUret;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label lblSkor;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label lblMayın;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AynıZamanlıUretim
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			lblSkor.Text = "0";
+			lblMayın.Text = "3";
+		}
+
+		private void btnUret_Click(object sender, EventArgs e)
+		{
+			int bomb1;
+			int bomb2;
+			int bomb3;
+
+			Random rnd = new Random();
+
+			bomb1 = rnd.Next(1,60);
+			bomb2 = rnd.Next(1,60);
+			bomb3 = rnd.Next(1,60);
+
+
+
+
+			for(int i =1;i<=60;i++)
+			{
+				Button btnTemp = new Button();
+
+				btnTemp.Name = "btn"+i.ToString();
+				btnTemp.Size = new System.Drawing.Size(35, 35);
+				btnTemp.TabIndex = 0;
+				btnTemp.UseVisualStyleBackColor = true;
+
+				if (bomb1 == i || bomb2 == i || bomb3 == i)
+				{
+					btnTemp.Tag = true;
+				}
+				else
+				{
+					btnTemp.Tag = false;
+				}
+				btnTemp.Click += BtnTemp_Click;
+				flowLayoutPanel1.Controls.Add(btnTemp);
+			}
+		}
+
+		
+		private void BtnTemp_Click(object sender, EventArgs e)
+		{
+			Button basilanButton = (Button)sender;
+			bool mayınBulunduMu = (bool)basilanButton.Tag;
+
+			if (mayınBulunduMu)
+			{
+				MessageBox.Show("Mayını bulundu!!!","UYARI");
+				basilanButton.BackgroundImage = Image.FromFile("bomb1.png");
+
+				
+
+				int mayın = int.Parse(lblMayın.Text);
+				mayın--;
+				lblMayın.Text = mayın.ToString();
+				int skor = int.Parse(lblSkor.Text);
+				skor-=10;
+				lblSkor.Text = skor.ToString();
+
+
+
+				if (mayın == 0)
+				{
+					MessageBox.Show("OYUN BİTTİ", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+					this.Close();
+				}
+			}
+			else
+			{
+				basilanButton.BackColor = Color.Green;
+
+				int skor = int.Parse(lblSkor.Text);
+				skor++;
+				lblSkor.Text = skor.ToString();
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
